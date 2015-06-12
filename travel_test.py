c66611eafbcabe2fecf6fa81b86403c91fbd8457
HaoFu <jamesfuhao@gamil.com>
2015-06-11 20:14:08 -0700

    change the structure

--- travel_test.py
+++ travel_test.py
@@ -5,6 +5,7 @@ from uiautomator import device as dev
 import xml.dom.minidom
 import os
 import time
+import difflib
 
 print dev.info
 dev.screen.on()
@@ -23,9 +24,14 @@ def nodelist_redef(nodelist):
     # only retrieve GUI components belong to target pkg
     # and only interested attributes enough to figure out the unique one
     nodelist_new = []
+    flag = True
     #tmpi = []
     for i in range(len(nodelist)):
         node = nodelist[i]
+        if flag and node.getAttribute('text'):
+            print "TTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTOPIC"
+            print node.getAttribute('text')
+            flag = False
         if node.getAttribute('clickable') == 'true':
             # node_index = node.getAttribute('index')
             node_text = node.getAttribute('text')
@@ -78,47 +84,84 @@ def nodelist_judge_redef(nodelist):
 def hash_window(nodelist):
     return hash(str(nodelist))
 
-def is_new_window():
-    global window_visted, window_node_map, same_window_counter
-    global nodelist
+def window_nodes_map(current_window, nodelist):
+    window = []
+    tmp = 0
+    for i in current_window:
+        nodeful = nodelist[tmp]
+        [node, j] = is_sub_node(nodeful, shown_clickable_details)
+        if node != -1:
+            node_index = shown_clickable_details.index(j)
+            shown_clickable_details[node_index] = nodeful
+            if shown_clickable_width[node_index] > 0:
+                window.append(node_index)
+        else:
+            shown_clickable.append(i)
+            shown_clickable_width.append(1)
+            shown_clickable_details.append(nodelist[tmp])
+            window.append(len(shown_clickable) - 1)
+        tmp += 1
+    return window
+
+def is_common_node(node1, node2):
+    node1 = [node1[0], node1[1], node1[2], node1[3], node1[6]]
+    node2 = [node2[0], node2[1], node2[2], node2[3], node2[6]]
+    if difflib.SequenceMatcher(None, str(node1), str(node2)).ratio() > 0.95:
+        return True
+
+def is_sub_node(node, nodelist):
+    for i in nodelist:
+        if is_common_node(node, i):
+            return [node, i]
+    return [-1, -1]
+
+def is_new_window(nodelist):
+    global window_visited, window_parent, same_window_counter
     current_window = nodelist_judge_redef(nodelist)
     tmp = hash_window(current_window)
-    if not window_visted:
-        window_visted.append(tmp)
-        window_node_map.append(-1)
-        return 0
+    window = window_nodes_map(current_window, nodelist)
+    if not window_visited:
+        window_visited.append(tmp)
+        window_parent.append(-1)
+        return [0, window]
     # return the width
-    if tmp not in window_visted:
+    if tmp not in window_visited:
+        for j in window_visited_details:
+            print difflib.SequenceMatcher(None, str(nodelist), str(j)).ratio()
+            if difflib.SequenceMatcher(None, str(nodelist), str(j)).ratio() > 0.5:
+                print 'gotTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTT you!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!'
+                same_window_counter += 1
+                tmpindex = window_visited_details.index(j)
+                newwindow = []
+                for k in window:
+                    node = shown_clickable_details[k]
+                    if is_sub_node(node, j) != [-1, -1]:
+                        newwindow.append(k)
+                if not newwindow:
+                    print window
+                    print nodelist
+                    print j
+                    exit(1)
+                return [window_parent[tmpindex], newwindow]
         same_window_counter = 0
         prinode = clicked_seq[len(clicked_seq) - 1]
+        print prinode
         if prinode != -1:
-            prinode_index = clicked_list.index(prinode)
-            if prinode_index not in window_node_map:
-                tmpcounter = 0
-                tnodelist = []
-                tmpc = 0
-                for i in current_window:
-                    if i in clicked_list:
-                        node_index = clicked_list.index(i)
-                        if clicked_list_width[node_index] > 0:
-                            tmpcounter += 1
-                            tnodelist.append(nodelist[tmpc])
-                            print i
-                    else:
-                        tmpcounter += 1
-                        tnodelist.append(nodelist[tmpc])
-                        print i
-                    tmpc += 1
-                clicked_list_width[prinode_index] = tmpcounter
-                window_visted.append(tmp)
-                window_node_map.append(prinode_index)
-                nodelist = tnodelist
+            if prinode not in window_parent:
+                shown_clickable_width[prinode] = len(window)
+                print '++++++'
+                print len(window)
+                print '++++++'
+                window_visited.append(tmp)
+                window_parent.append(prinode)
+                window_visited_details.append(nodelist)
                 print 'NEEEEEEEEEEEEEEEEEEEEEEEW WINDDDDDDDDDDDDDDDDDDDOW'
-                return prinode_index
+
+        return [prinode, window]
     else:
         same_window_counter += 1
-        tmpindex = window_visted.index(tmp)
-        return window_node_map[tmpindex]
+        tmpindex = window_visited.index(tmp)
+        return [window_parent[tmpindex], window]
 
 
 def check_clicked():
@@ -138,76 +181,70 @@ def check_clicked():
     nodelist = nodelist_redef(nodelist)
 
     # num of nodes at new window is the width of the parent node
-    parent = is_new_window()
+    [parent, window] = is_new_window(nodelist)
+    print window
     #print 'parent'
     #print parent
     #print ' .........'
 
-    print clicked_list_width
-    for i in nodelist:
-        if same_window_counter > 10:
-            break
-        node = judge_click_attributes(i)
-        if node in clicked_list:
-            node_index = clicked_list.index(node)
-            if clicked_list_width[node_index] > 0:
-                print node
-                if node == clicked_seq[len(clicked_seq) - 1]:
-                    clicked_list_width[node_index] = 0
-                ui_interaction(i)
-                clicked_list_width[node_index] -= 1
-                if parent:
-                    clicked_list_width[parent] -= 1
-                clicked_seq.append(node)
-                print 'what!!!!!!!!!!!!!!!!!!'
-                print clicked_list_width[node_index]
-                return True
-        else:
-            print node
-            if parent:
-                clicked_list_width[parent] -= 1
-            clicked_seq.append(node)
-            clicked_list.append(node)
-            clicked_list_width.append(0)
-            ui_interaction(i)
-            return True
+    print shown_clickable_width
+
+    if not window_visited:
+        print 'ffffffffffffffffffffffffffff'
+        return True
+    if not window or same_window_counter > 30:
+        clicked_seq.append(-1)
+        dev.press.back()
+        global back_counter
+        print "back2"
+        back_counter += 1
+        return False
+    for i in window:
+        if parent:
+            shown_clickable_width[parent] -= 1
+        if clicked_seq != [] and i == clicked_seq[len(clicked_seq) - 1]:
+            shown_clickable_width[i] = 0
+            continue
+        clicked_seq.append(i)
+        shown_clickable_width[i] -= 1
+        ui_interaction(shown_clickable_details[i])
+        return True
     if parent > 0:
-        clicked_list_width[parent] = 0
-    clicked_seq.append(-1)
-    dev.press.back()
-    global back_counter
-    print "back2"
-    back_counter += 1
-    return False
+            shown_clickable_width[parent] = 0
+
+
 
 
 def ui_interaction(node):
     arg = get_selector_attributes(node)
     if not dev(**arg).exists:
+        print 'not found'
         return
     cmd = 'CLICK'
     print arg
     CMD_MAP[cmd](dev, arg)
-    print 'click ' + node[2] + ' at ' + current_time
+    print 'click ' + node[0] + ' at ' + current_time
 
 
 package = 'com.google.android.deskclock'
 #package = 'com.android.dialer'
 #package = 'com.android.settings'
 #package = 'com.android.deskclock'
-clicked_list = []
+shown_clickable = []
+shown_clickable_details = []
 clicked_seq = []
-nodelist = []
 back_counter = 0
-clicked_list_width = []
-window_visted = []
-window_node_map = []
+shown_clickable_width = []
+window_visited = [] # save the hash
+window_visited_details = [] # save the nodes it contained
+window_parent = [] # parent node of the window
 tic = time.clock()
 same_window_counter = 0
 # per activity (not necessary a new activity, new updated window is enough)
 while check_clicked() or back_counter <= 15:
     toc = time.clock()
-    time.sleep(1)
+    #if clicked_seq[len(clicked_seq) - 1] == -1:
+
     #print toc - tic
     tic = time.clock()
     pass
